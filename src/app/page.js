627b54215cc77daca83bc7520ec183933e31f1b6
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-12-04 11:55:50 +0200

    add header content

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,97 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Header from "@/components/header/header";
+import style from "./page.module.css";
 
 export default function Home() {
-  return Header;
+  let countries = [
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+    "اسم الدولة",
+  ];
+
+  return (
+    <>
+      <div className="container-fuild bg-light">
+        <div className="container pt-5 pb-3">
+          <div className="row">
+            {countries.map((country, index) => (
+              <div
+                key={index}
+                className={`col-lg-3 col-md-6 col-sm-12 mb-4 gap-2${style.card}`}
+              >
+                <div
+                  role="button"
+                  className={`d-flex justify-content-center align-items-center shadow-sm rounded-1 text-center gap-2 py-2 px-3 bg-white `}
+                >
+                  <div>{country}</div>
+                  <div>
+                    <img
+                      className="rounded-1"
+                      src="https://via.placeholder.com/20"
+                      alt={`${country} placeholder`}
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="text-center">
+            <small
+              role="button"
+              className=" py-2 px-5 bg-dark text-light rounded-1 mt-4"
+            >
+              عرض جميع الدول
+            </small>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="container-fluid mt-3 mb-3">
+        <div className="container">
+          <p className="text-dark">"كيف تستخدم تجوال</p>
+          <div className="row m-5">
+            <div className="col-lg-3">
+              <img
+                className="rounded-1"
+                src="https://via.placeholder.com/200x400"
+                alt="phone picture"
+              />
+            </div>{" "}
+            <div className="col-lg-3">
+              <img
+                className="rounded-1"
+                src="https://via.placeholder.com/200x400"
+                alt="phone picture"
+              />
+            </div>{" "}
+            <div className="col-lg-3">
+              <img
+                className="rounded-1"
+                src="https://via.placeholder.com/200x400"
+                alt="phone picture"
+              />
+            </div>{" "}
+            <div className="col-lg-3">
+              <img
+                className="rounded-1"
+                src="https://via.placeholder.com/200x400"
+                alt="phone picture"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,85 @@
-"use server";
+"use client";
 
-export default function Header() {
-  return <h1>header</h1>;
-}
+import React from "react";
+import styles from "./header.module.css";
+import Link from "next/link";
+import useState from "react";
+const Header = () => {
+  return (
+    <header dir="rtl" className={styles.header}>
+      <nav className={styles.navbar}>
+        {/* Logo */}
+        <div className={styles.logo}>
+          تجوال <br /> Tajwal
+        </div>
+
+        {/* Nav Items */}
+        <ul className={styles.navItems}>
+          <li>
+            <Link className="text-dark" href="#">
+              البداية
+            </Link>
+          </li>
+          <li>
+            <Link href="#">الدول</Link>
+          </li>
+          <li>
+            <Link href="#">العروض</Link>
+          </li>
+          <li>
+            <Link href="#">مركز المساعدة</Link>
+          </li>
+          <li>
+            <Link href="#">اتصل بنا</Link>
+          </li>
+          <li>
+            <Link href="#">
+              <small>english</small>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <small> ر.س</small>
+            </Link>
+          </li>
+        </ul>
+
+        {/* Login/Sign In */}
+        <div className={styles.auth}>
+          <button className={`${styles.loginBtn} rounded`}>دخول</button>
+          <button className={`${styles.signinBtn} rounded`}>تسجيل</button>
+        </div>
+      </nav>
+      <div className="text-center mt-5">
+        <h2>ابقي متصلا اينما كنت</h2>
+        <p>في اكثر من 200 دولة</p>
+        <div className=" d-flex justify-content-center">
+          <div className="col-lg-6">
+            <input placeholder="ابحث" className="col-lg-12 form-control" />
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center mt-4">
+        <ul className={`${styles.navItems} text-center`}>
+          <li>
+            <Link className="bg-light px-5 py-2 text-black rounded" href="#">
+              دولية
+            </Link>
+          </li>
+          <li>
+            <Link className=" px-5 py-2" href="#">
+              قارية
+            </Link>
+          </li>
+          <li>
+            <Link className="px-5 py-2" href="#">
+              عالمية
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
