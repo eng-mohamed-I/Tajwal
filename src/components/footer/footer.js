d797a62bf665e36cce9f015fb4732dd2773459b7
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-12-04 12:00:27 +0200

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,1 +1,13 @@
-const
+const Footer = () => {
+  return (
+    <div className="bg-dark">
+        <div></div>
+      <div className="d-flex">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
